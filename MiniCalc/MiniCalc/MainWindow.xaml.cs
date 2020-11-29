9ab0bd22e3e364fd96fe7673237bd94f50a32388
viviankamo <vivian.kamo@elev.ga.ntig.se>
2020-11-29 22:36:19 +0100

    calc

--- MiniCalc/MiniCalc/MainWindow.xaml.cs
+++ MiniCalc/MiniCalc/MainWindow.xaml.cs
@@ -21,9 +21,245 @@ namespace MiniCalc
     public partial class MainWindow : Window
     {
 
+        long nr1 = 0;
+        long nr2 = 0;
+        string input = "";
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10) + 1;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10) + 1;
+                txtDisplay.Text = nr2.ToString();
+
+            }
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10) + 2;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10) + 2;
+                txtDisplay.Text = nr2.ToString();
+
+            }
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10) + 3;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10) + 3;
+                txtDisplay.Text = nr2.ToString();
+            }
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10) + 4;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10) + 4;
+                txtDisplay.Text = nr2.ToString();
+            }
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10) + 5;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10) + 5;
+                txtDisplay.Text = nr2.ToString();
+            }
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10) + 6;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10) + 6;
+                txtDisplay.Text = nr2.ToString();
+            }
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10) + 7;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10) + 7;
+                txtDisplay.Text = nr2.ToString();
+            }
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10) + 8;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10) + 8;
+                txtDisplay.Text = nr2.ToString();
+            }
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10) + 9;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10) + 9;
+                txtDisplay.Text = nr2.ToString();
+            }
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 * 10);
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 * 10);
+                txtDisplay.Text = nr2.ToString();
+            }
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            input = "+";
+            txtDisplay.Text = "0";
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            input = "-";
+            txtDisplay.Text = "0";
+
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            input = "*";
+            txtDisplay.Text = "0";
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            input = "/";
+            txtDisplay.Text = "0";
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            switch (input)
+            {
+                case "+":
+                    txtDisplay.Text = (nr1 + nr2).ToString();
+                    break;
+                case "-":
+                    txtDisplay.Text = (nr1 - nr2).ToString();
+                    break;
+                case "*":
+                    txtDisplay.Text = (nr1 * nr2).ToString();
+                    break;
+                case "/":
+                    txtDisplay.Text = (nr1 / nr2).ToString();
+                    break;
+            }
+        }
+
+        private void btnPlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 *= -1;
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 *= -1;
+                txtDisplay.Text = nr2.ToString();
+
+            }
+        }
+
+        private void btnAC_Click(object sender, RoutedEventArgs e)
+        {
+            nr1 = 0;
+            nr2 = 0;
+            input = "";
+            txtDisplay.Text = "0";
+        }
+
+        private void btnComma_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "."; 
+        }
+
+        private void btnBackSpace_Click(object sender, RoutedEventArgs e)
+        {
+            if (input == "")
+            {
+                nr1 = (nr1 / 10);
+                txtDisplay.Text = nr1.ToString();
+            }
+            else
+            {
+                nr2 = (nr2 / 10);
+                txtDisplay.Text = nr2.ToString();
+
+            }
+        }
     }
 }
+
+
+
